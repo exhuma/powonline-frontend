bf2d83efdb75e91488e21c1310730519ed319682
Michel Albert <michel@albert.lu>
2018-02-25 17:08:26 +0100

    Freeze production URL of API

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const siteSettings = require('./siteSettings.env')
 const pusher = require('./pusher.env')
 module.exports = merge({
-  NODE_ENV: '"production"'
-}, siteSettings, pusher)
+  NODE_ENV: '"production"',
+  BACKEND_URL: '"https://powonline-api.albert.lu"'
+}, pusher)
