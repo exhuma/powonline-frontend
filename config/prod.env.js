281bc9f6d03fc6b3f27a9d751a29f3d0d70a0b14
Michel Albert <michel@albert.lu>
2018-02-25 19:35:20 +0100

    Remove unused import

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,5 @@
 'use strict'
 const merge = require('webpack-merge')
-const siteSettings = require('./siteSettings.env')
 const pusher = require('./pusher.env')
 module.exports = merge({
   NODE_ENV: '"production"',
