42f1cf74de645369ebce7139bc0f8222c585b49c
Michel Albert <michel@albert.lu>
2018-02-26 18:18:37 +0100

    Use popup-dialog for new user form

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,33 +1,26 @@
 <template>
   <center-col id="UserList">
-    <transition name="slide">
-      <v-card v-show="isAddBlockVisible">
-        <v-card-title>
-          <span>Add New User</span>
-          <v-spacer></v-spacer>
-          <v-btn @click.native="closeAddBlock" icon><v-icon>close</v-icon></v-btn>
-        </v-card-title>
-        <v-card-text>
-          <v-text-field
-            name="user-input"
-            id="UserNameImput"
-            @keyup.enter.native="addUser"
-            type='text'
-            v-model='username'
-            label='Enter a new username' />
-          <v-text-field
-            name="password"
-            @keyup.enter.native="addUser"
-            type='password'
-            v-model='password'
-            label='Password' />
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn @click.native="addUser" flat>Add</v-btn>
-        </v-card-actions>
-      </v-card>
-    </transition>
+
+    <popup-dialog
+      @dialogConfirmed="addUser"
+      @dialogDismissed="closeAddBlock"
+      :dialogVisible="isAddBlockVisible"
+      title="Add new User">
+      <v-text-field
+        name="user-input"
+        id="UserNameImput"
+        @keyup.enter.native="addUser"
+        type='text'
+        v-model='username'
+        label='Enter a new username' />
+      <v-text-field
+        name="password"
+        @keyup.enter.native="addUser"
+        type='password'
+        v-model='password'
+        label='Password' />
+    </popup-dialog>
+
     <user-block v-for="user in users" :name="user.name" :key="user.name"></user-block>
   </center-col>
 </template>
@@ -41,9 +34,7 @@ export default {
         name: this.username,
         password: this.password
       })
-      const input = document.getElementById('UserNameImput')
-      input.focus()
-      input.select()
+      this.$store.commit('closeAddBlock', this.$route.path)
     },
     closeAddBlock () {
       this.$store.commit('closeAddBlock', this.$route.path)
