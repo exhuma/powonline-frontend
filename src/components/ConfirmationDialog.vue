7317c324faf18f994193405ac5fa255d8a5a64b4
Michel Albert <michel@albert.lu>
2018-02-27 18:26:33 +0100

    Improve button layout on confirmation dialog

--- src/components/ConfirmationDialog.vue
+++ src/components/ConfirmationDialog.vue
@@ -5,8 +5,9 @@
       <v-card-title><slot name="title">Confirm Action</slot></v-card-title>
       <v-card-text><slot name="text">Are you sure?</slot></v-card-text>
       <v-card-actions>
-        <v-btn @click.native="discardAction">No</v-btn>
-        <v-btn @click.native="acceptAction">Yes</v-btn>
+        <v-spacer />
+        <v-btn flat @click.native="discardAction">No</v-btn>
+        <v-btn class="error" @click.native="acceptAction">Yes</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
