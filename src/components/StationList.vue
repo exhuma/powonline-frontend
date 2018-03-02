7201281d2bafc3cf26a6cc149b6edd9dad399325
Michel Albert <michel@albert.lu>
2018-03-02 12:30:38 +0100

    Encapsulate "add" block into station list

--- src/components/StationList.vue
+++ src/components/StationList.vue
@@ -1,14 +1,14 @@
 <template>
   <center-col id="StationList">
     <popup-dialog
-      @dialogConfirmed="addStation"
+      @dialogConfirmed="onDialogConfirmed"
       @dialogDismissed="closeAddBlock"
       :dialogVisible="isAddBlockVisible"
       title="Add New Station">
       <v-text-field
-        @keyup.enter.native="addStation"
+        @keyup.enter.native="onDialogConfirmed"
         type='text'
-        v-model='stationname'
+        v-model='selectedStation.name'
         label='Enter a new stationname' />
     </popup-dialog>
     <v-list two-line>
@@ -16,22 +16,55 @@
         v-for="station in stations"
         :name="station.name"
         :key="station.name"></station-block>
+      <v-list-tile>
+        <v-spacer />
+        <v-list-tile-action v-if="hasRole('admin')">
+          <v-btn @click="openCreateDialog" v-if="hasRole('admin')">Add new Station</v-btn>
+        </v-list-tile-action>
+      </v-list-tile>
     </v-list>
   </center-col>
 </template>
 
 <script>
+import model from '@/model'
+
 export default {
   name: 'station_list',
   methods: {
-    addStation: function (event) {
-      this.$store.dispatch('addStationRemote', {
-        name: this.stationname
-      })
-      this.$store.commit('closeAddBlock', this.$route.path)
+    onOpenEditDialog: function (station) {
+      this.selectedStation = station
+      this.isAddBlockVisible = true
+      this.sendMode = model.SEND_MODE.UPDATE
+    },
+    onDialogConfirmed: function (event) {
+      const station = this.selectedStation
+
+      if (this.sendMode === model.SEND_MODE.CREATE) {
+        this.$store.dispatch('addStationRemote', station)
+      } else if (this.sendMode === model.SEND_MODE.UPDATE) {
+        console.warn('Updating stations is not implemented yet!')
+      } else {
+        console.error('Invalid send mode: ' + this.sendMode)
+      }
+
+      this.$emit('stationSaved', station)
+      this.selectedStation = model.station.makeEmpty()
+
+      this.isAddBlockVisible = false
     },
     closeAddBlock () {
-      this.$store.commit('closeAddBlock', this.$route.path)
+      this.isAddBlockVisible = false
+    },
+    openCreateDialog: function () {
+      const newStation = model.station.makeEmpty()
+
+      this.selectedStation = newStation
+      this.isAddBlockVisible = true
+      this.sendMode = model.SEND_MODE.CREATE
+    },
+    hasRole (roleName) {
+      return this.$store.state.roles.indexOf(roleName) > -1
     }
   },
   created () {
@@ -39,15 +72,14 @@ export default {
   },
   data () {
     return {
-      stationname: ''
+      isAddBlockVisible: false,
+      selectedStation: model.station.makeEmpty(),
+      sendMode: model.SEND_MODE.CREATE
     }
   },
   computed: {
     stations () {
       return this.$store.state.stations
-    },
-    isAddBlockVisible () {
-      return this.$store.state.isAddBlockVisible[this.$route.path]
     }
   }
 }
