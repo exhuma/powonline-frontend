0657de86f705a6e405d6cde3671d3da6cf96b9bd
Michel Albert <michel@albert.lu>
2018-03-02 12:30:08 +0100

    Centralise default entity data in model/index.js

---
+++ src/model/index.js
@@ -0,0 +1,41 @@
+export default {
+
+  SEND_MODE: {
+    CREATE: 'create',
+    UPDATE: 'update'
+  },
+
+  station: {
+    makeEmpty: function () {
+      return {
+        name: ''
+      }
+    }
+  },
+
+  team: {
+    makeEmpty: function () {
+      return {
+        name: '',
+        email: 'nobody@example.com', // TODO we should make this optional
+        order: 500,
+        cancelled: false,
+        contact: '',
+        phone: '',
+        comments: '',
+        is_confirmed: false,
+        confirmation_key: '',
+        accepted: false,
+        completed: false,
+        inserted: null,
+        updated: null,
+        num_vegetarians: 0,
+        num_participants: 0,
+        planned_start_time: null,
+        effective_start_time: null,
+        finish_time: null,
+        route_name: ''
+      }
+    }
+  }
+}
