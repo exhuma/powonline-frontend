2228f571a85d9b33e4a19f16987778b30d3a35a9
Michel Albert <michel@albert.lu>
2018-05-11 08:11:33 +0200

    Update production host

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,7 +4,7 @@ const pusher = require('./pusher.env')
 const oauth = require('./oauth.env')
 module.exports = merge({
   NODE_ENV: '"production"',
-  BACKEND_URL: '"https://powonline-api.albert.lu"',
+  BACKEND_URL: '"https://lost2018-api.albert.lu"',
   PUSHER_DEBUG: 'false',
   PUSHER_CHANNEL: '"team-station-state"'
 }, pusher, oauth)
