392b0b2a763a1e48048529abee4521ccd223e40b
Michel Albert <michel@albert.lu>
2019-05-03 23:43:29 +0200

    Version bumped to 2019.05.1

--- src/App.vue
+++ src/App.vue
@@ -107,7 +107,7 @@ export default {
       globalSnack: false,
       globalSnackText: '',
       globalSnackColor: '',
-      version: '2019.04.3'
+      version: '2019.05.1'
     }
   },
   methods: {
