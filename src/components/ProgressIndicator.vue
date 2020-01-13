e958a1ac7403517168c182e1c6f2185aa83bc3d8
Michel Albert <michel@albert.lu>
2020-01-13 20:21:23 +0100

    Add progress indicator as component

---
+++ src/components/ProgressIndicator.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-progress-linear
+      v-if="!model.visible"
+      class="mt-0"
+      height="4"></v-progress-linear>
+    <v-progress-linear
+      v-if="model.visible"
+      class="mt-0"
+      height="4"
+      v-model="model.progress"
+      :indeterminate="model.progress === -1"></v-progress-linear>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProgressIndicator',
+  props: ["model"]
+};
+</script>
