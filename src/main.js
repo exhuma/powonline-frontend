55fe691c2632ab5b8cd51bbfa00be73e8df232de
Michel Albert <michel@albert.lu>
2020-01-13 19:17:00 +0100

    Add config objects

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import development from "@/config/development"
+import production from "@/config/production"
 
 Vue.config.productionTip = false
+
+if (process.env.NODE_ENV === "production") {
+  Vue.prototype.$config = Object.freeze(production)
+} else {
+  Vue.prototype.$config = Object.freeze(development)
+}
 
 new Vue({
   vuetify,
