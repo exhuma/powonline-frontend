478c8d2fb869b46db769272505a2e56d03ddbc4b
Michel Albert <michel@albert.lu>
2020-02-26 12:06:53 +0100

    Fix PopupDialog for new Vue version

--- src/components/PopupDialog.vue
+++ src/components/PopupDialog.vue
@@ -4,21 +4,20 @@
     transition="dialog-bottom-transition"
     :overlay="false"
     scrollable
-    fullscreen
     max-width="100em"
     >
     <v-card>
-      <v-toolbar card>
+      <v-toolbar dark>
         <v-toolbar-title>{{title}}</v-toolbar-title>
         <v-spacer />
-        <v-btn @click="dismiss" icon><v-icon>close</v-icon></v-btn>
+        <v-btn @click="dismiss" icon><v-icon>mdi-close</v-icon></v-btn>
       </v-toolbar>
       <v-card-text>
         <slot>Dialog Content</slot>
       </v-card-text>
       <v-card-actions v-show="!customActionButtons">
         <v-spacer></v-spacer>
-        <v-btn flat @click="dismiss">Cancel</v-btn>
+        <v-btn text @click="dismiss">Cancel</v-btn>
         <v-btn @click="confirm">{{(editMode === true ? 'Save' : 'Add')}}</v-btn>
       </v-card-actions>
     </v-card>
