bd9c83a8f4b96e5c245c1efc86cbda63f3614bf0
Michel Albert <michel@albert.lu>
2020-02-26 11:27:29 +0100

    Add a separate component for a station form

---
+++ src/components/StationForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-text-field
+      type='text'
+      v-model='station.name'
+      label='Enter a new stationname' />
+    <v-text-field
+      name="order"
+      type='number'
+      v-model='station.order'
+      hint="This field is used to sort stations"
+      label="Station Ordering" />
+    <v-checkbox
+      name="is_start"
+      label="Departure Station"
+      v-model="station.is_start" />
+    <v-checkbox
+      name="is_end"
+      label="Arrival Station"
+      v-model="station.is_end" />
+    <v-text-field
+      name="phone"
+      v-model='station.phone'
+      label="Phone Number" />
+    <v-text-field
+      name="contact"
+      v-model='station.contact'
+      label="Contact" />
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'StationForm',
+  props: {
+    station: {
+      type: Object
+    }
+  }
+};
+</script>
