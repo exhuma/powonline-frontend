78ef9ab23a4e333e70670720248928a7915a608e
Michel Albert <michel@albert.lu>
2020-03-02 12:49:49 +0100

    Add a dummy debug page for some dev-testing

---
+++ src/components/Debug.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Debug</h1>
+    <table width="100%" border="1">
+      <tr>
+        <th>Key</th>
+        <th>Value</th>
+      </tr>
+      <tr>
+        <td>Store Identity</td>
+        <td>{{ storeIdentity || 'N/A' }}</td>
+      </tr>
+      <tr>
+        <td>Identity</td>
+        <td>{{ identity || 'N/A'}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Debug',
+  props: [
+    "identity"
+  ],
+  computed: {
+    storeIdentity: function () {
+      return this.$store.state.identity
+    }
+  }
+};
+</script>
