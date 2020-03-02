394c2487732ac77f769e02fa3233d2f87c7945df
Michel Albert <michel@albert.lu>
2020-03-02 12:45:33 +0100

    Add support to filter routes by authorisation role

--- src/router/index.js
+++ src/router/index.js
@@ -4,91 +4,140 @@ import Gallery from '@/components/Gallery';
 import Scoreboard from '@/components/Scoreboard';
 import StationList from '@/components/StationList';
 import StationDashboard from '@/components/StationDashboard';
+import UserList from '@/components/UserList';
+import Debug from '@/components/Debug';
 
 
-function getRoutes(userRoles) {
+function getRoutes() {
   const output = [
+    {
+      path: '/team/:teamName',
+      component: NotFound,
+      // TODO component: TeamPanel
+      inMenu: false,
+      availableToAnonymous: true
+    },
+    {
+      path: '/debug',
+      label: 'debug',
+      icon: 'mdi-wrench',
+      component: Debug,
+      inMenu: true,
+      availableToAnonymous: false,
+      needsRole: 'admin'
+    },
     {
       path: '/station/:stationName',
       component: StationDashboard,
-      inMenu: false
+      inMenu: false,
+      availableToAnonymous: true
     },
     {
       path: '/matrix',
       label: 'Dashboard',
       icon: 'mdi-border-all',
       component: Dashboard,
-      inMenu: true
+      inMenu: true,
+      availableToAnonymous: true
     },
     {
       path: '/scoreboard',
       label: 'Scoreboard',
       icon: 'mdi-format-list-numbered',
       component: Scoreboard,
-      inMenu: true
+      inMenu: true,
+      availableToAnonymous: true
     },
     {
       path: '/gallery',
       label: 'Photos',
       icon: 'mdi-image',
       component: Gallery,
-      inMenu: true
-    }
-  ]
-  if (userRoles.length > 0) {
-    output.push({
+      inMenu: true,
+      availableToAnonymous: true
+    },
+    {
       path: '/station',
       label: 'Stations',
       icon: 'mdi-map-marker',
       component: StationList,
-      inMenu: true
-    })
-    output.push({
+      inMenu: true,
+      availableToAnonymous: false
+    },
+    {
       path: '/team',
       label: 'Teams',
       icon: 'mdi-group',
       component: NotFound,
-      inMenu: true
-    })
-    output.push({
+      inMenu: true,
+      availableToAnonymous: false
+    },
+    {
       path: '/uploads',
       label: 'Uploads',
       icon: 'mdi-file-upload',
       component: NotFound,
-      inMenu: true
-    })
-  }
-  if (userRoles && userRoles.indexOf('admin') > -1) {
-    output.push({
+      inMenu: true,
+      availableToAnonymous: false
+    },
+    {
       path: '/route',
       label: 'Routes',
       icon: 'mdi-gesture',
       component: NotFound,
-      inMenu: true
-    })
-    output.push({
+      inMenu: true,
+      availableToAnonymous: false,
+      needsRole: 'admin'
+    },
+    {
       path: '/user',
       label: 'Users',
       icon: 'mdi-face',
-      component: NotFound,
-      inMenu: true
-    })
-    output.push({
+      component: UserList,
+      inMenu: true,
+      availableToAnonymous: false,
+      needsRole: 'admin'
+    },
+    {
       path: '/auditlog',
       label: 'Audit',
       icon: 'mdi-receipt',
       component: NotFound,
-      inMenu: true
-    })
-  }
-  output.push({
-    path: '/changelog',
-    label: 'Changelog',
-    icon: 'mdi-playlist-check',
-    component: NotFound,
-    inMenu: true
-  })
+      inMenu: true,
+      availableToAnonymous: false,
+      needsRole: 'admin'
+    },
+    {
+      path: '/changelog',
+      label: 'Changelog',
+      icon: 'mdi-playlist-check',
+      component: NotFound,
+      inMenu: true,
+      availableToAnonymous: false,
+    }
+  ]
   return output
 }
 
-export {getRoutes}
+/**
+ * Returns only a list of accessible routes.
+ *
+ * If any of the given roles matches the role needed in a route, it will be
+ * included in he output.
+ *
+ * @param roles A list of user-roles
+ */
+function accessibleRoutes (roles) {
+  const allRoutes = getRoutes()
+  const accessible = allRoutes.filter(({needsRole}) => {
+    if (needsRole === undefined || needsRole === null) {
+      return true
+    }
+    let idx = roles.findIndex((haveRole) => {return haveRole === needsRole})
+    return idx > -1
+  })
+  const inMenu = accessible.filter(({inMenu}) => {return inMenu})
+  return inMenu
+}
+
+export {getRoutes, accessibleRoutes}
