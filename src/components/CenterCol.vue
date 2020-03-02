4ee5aec889c5a3f8465eceb88bfa3e2564b3f5f4
Michel Albert <michel@albert.lu>
2020-03-02 15:54:24 +0100

    Add a new "contatiner" to cente cols

--- src/components/CenterCol.vue
+++ src/components/CenterCol.vue
@@ -1,9 +1,11 @@
 <template>
-  <v-layout row wrap justify-center>
-    <v-flex xs12 md8 lg6 xl4>
-      <slot>Centered content</slot>
-    </v-flex>
-  </v-layout>
+  <v-container>
+    <v-layout row wrap justify-center>
+      <v-flex xs12 md8 lg6 xl4>
+        <slot>Centered content</slot>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
