92aede6b47967989d51abd5a4c3efa5fec510046
Michel Albert <michel@albert.lu>
2022-03-30 07:08:44 +0200

    Recover auditlog component

---
+++ src/components/AuditLog.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <v-text-field
+      v-model="entryFilter"
+      append-icon="search"
+      clearable
+      label="Filter"
+      @click:clear="onFilterCleared"
+      hint="Filter entries"
+      ></v-text-field>
+    <v-data-table
+      :headers="headers"
+      :items="filteredEntries">
+      <template v-slot:items="props">
+        <td>{{ format_ts(props.item.timestamp) }}</td>
+        <td>{{ props.item.username }}</td>
+        <td>{{ props.item.type }}</td>
+        <td>{{ props.item.message }}</td>
+      </template>
+    </v-data-table>
+    <v-btn @click="refresh()">Refresh</v-btn>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  name: 'auditlog',
+  data () {
+    return {
+      entries: [],
+      entryFilter: '',
+      headers: [
+        {text: 'Timestamp', sortable: false},
+        {text: 'User', sortable: false},
+        {text: 'Type', sortable: false},
+        {text: 'Message', sortable: false}
+      ]
+    }
+  },
+  computed: {
+    filteredEntries () {
+      let all = this.entries
+      let filtered = null
+      if (!this.entryFilter || this.entryFilter.length < 3) {
+        filtered = all
+      } else {
+        filtered = all.filter((item) => {
+          let fltr = this.entryFilter.toLowerCase()
+          let userMatches = item.username.toLowerCase().includes(fltr)
+          let typeMatches = item.type.toLowerCase().includes(fltr)
+          let msgMatches = item.message.toLowerCase().includes(fltr)
+          return userMatches || typeMatches || msgMatches
+        })
+      }
+      return filtered
+    }
+  },
+  methods: {
+    format_ts (ts) {
+      let obj = moment(ts)
+      return obj.format('YYYY-MM-DD HH:mm:ss')
+    },
+    refresh () {
+      this.$remoteProxy.fetchAuditLog()
+        .then(result => {
+          this.entries = result
+        })
+        .catch(e => {
+          console.error(e)
+          this.$emit('snackRequested', {
+            message: `Unable to update audit-log (${e.response.data})`,
+            color: 'red'
+          })
+        })
+    },
+    onFilterCleared (e) {
+      this.entryFilter = ''
+    }
+  },
+  created () {
+    this.refresh()
+  }
+}
+</script>
