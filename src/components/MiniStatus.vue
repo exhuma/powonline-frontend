5e2a947fdedbc432af36110635ea9afc890137fe
Michel Albert <michel@albert.lu>
2022-03-30 07:12:28 +0200

    Recover MiniStatus component from old-develop

---
+++ src/components/MiniStatus.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-icon>{{ stateIcon }}</v-icon>
+</template>
+
+<script>
+import util from '@/util'
+export default {
+  name: 'mini-status',
+  props: ['team', 'station'],
+  data () {
+    return {
+      state: 'unknown',
+      stateIcon: 'u'
+    }
+  },
+  created () {
+    this.$remoteProxy.fetchTeamState(this.station, this.team).then((state) => {
+      const icon = util.getStateIcon(state)
+      this.stateIcon = icon
+    }).catch(e => {
+      this.$store.commit('logError', e)
+    })
+  }
+}
+</script>
