d6bc929df86cae9d940b2ac4134bf1041f3fa8cf
Michel Albert <michel@albert.lu>
2022-03-30 07:11:19 +0200

    Recover global-dashboard from old-develop

---
+++ src/components/GlobalDashboard.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <v-container id="GlobalDashboard">
+    <v-layout row wrap>
+      <v-flex xs12>
+
+        <route-dashboard
+          v-for="route in routes"
+          class="mb-5"
+          :route="route"
+          :key="route.name"></route-dashboard>
+
+      </v-flex>
+    </v-layout>
+  </v-container>
+
+</template>
+
+<script>
+export default {
+  name: 'global_dashboard',
+  computed: {
+    routes () {
+      return this.$store.state.routes
+    }
+  },
+  created () {
+    this.$store.dispatch('refreshGlobalDashboard')
+    this.$store.dispatch('fetchQuestionnaireScores')
+    this.$store.commit('changeTitle', 'Global Dashboard')
+  }
+}
+</script>
