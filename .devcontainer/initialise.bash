7f561e0f52dcca05cd76332ddb939905aea6e576
Michel Albert <michel@albert.lu>
2023-04-11 19:07:43 +0200

    Fix materialisation of config-files

--- .devcontainer/initialise.bash
+++ .devcontainer/initialise.bash
@@ -2,5 +2,5 @@
 set -xe
 
 npm clean-install
-cp .config/pusher.env.js.dist .config/pusher.env.js
-cp .config/siteSettings.env.js.dist .config/siteSettings.env.js
+cp config/pusher.env.js.dist config/pusher.env.js
+cp config/siteSettings.env.js.dist config/siteSettings.env.js
