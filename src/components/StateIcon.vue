5085e88ccc025195fdad44ef897a1e3d329c6db0
Michel Albert <michel@albert.lu>
2023-05-01 15:24:41 +0200

    Colorise state icons

--- src/components/StateIcon.vue
+++ src/components/StateIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-icon>{{ stateIcon() }}</v-icon>
+  <v-icon :color="adaptiveColor">{{ stateIcon() }}</v-icon>
 </template>
 
 <script>
@@ -8,6 +8,19 @@ import util from '@/util';
 export default {
   name: 'state-icon',
   props: ['state'],
+  computed: {
+    adaptiveColor() {
+      switch (this.state) {
+        case 'arrived':
+          return 'yellow'
+        case 'finished':
+          return 'green'
+        default:
+          return 'white'
+      }
+
+    }
+  },
   methods: {
     stateIcon () {
        return util.getStateIcon(this.state)
