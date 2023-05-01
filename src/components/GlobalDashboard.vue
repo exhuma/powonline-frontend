43fa96410634fbde0fffa3c00feda9d4d170adbb
Michel Albert <michel@albert.lu>
2023-05-01 15:05:14 +0200

    Restore the old dashboard

--- src/components/GlobalDashboard.vue
+++ src/components/GlobalDashboard.vue
@@ -1,17 +1,42 @@
 <template>
   <v-container id="GlobalDashboard">
-    <combined-dashboard :routes="routes()"></combined-dashboard>
+    <v-tabs v-model="activeTab" grow>
+      <v-tab>Combined Dashboard</v-tab>
+      <v-tab>Separate Dashboard</v-tab>
+      <v-tab>Legacy Dashboard</v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="activeTab" class="pa-5">
+      <v-tab-item>
+        <combined-dashboard :routes="routes()"></combined-dashboard>
+      </v-tab-item>
+      <v-tab-item>
+        <route-dashboard
+          v-for="route in routes()"
+          :key="route.name"
+          :route="route"
+        ></route-dashboard>
+      </v-tab-item>
+      <v-tab-item>
+        <route-dashboard-legacy
+          v-for="route in routes()"
+          :key="route.name"
+          :route="route"
+        ></route-dashboard-legacy>
+      </v-tab-item>
+    </v-tabs-items>
   </v-container>
 </template>
 
 <script>
+import RouteDashboardLegacy from '@/components/RouteDashboardLegacy.vue'
 const AUTO_REFRESH_INTERVAL_SECONDS = 10
 export default {
   name: 'global_dashboard',
   data () {
     return {
       refreshId: null,
-      pctUntilNextRefresh: 100.0
+      pctUntilNextRefresh: 100.0,
+      activeTab: 0
     }
   },
   methods: {
@@ -50,6 +75,9 @@ export default {
   },
   beforeDestroy () {
     this.stopAutoRefresh()
+  },
+  components: {
+    'route-dashboard-legacy': RouteDashboardLegacy
   }
 }
 </script>
